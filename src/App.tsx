ec2e0f1beab68ae5951e0fa01964aed6f4074b7c
1 <yuyi.gz@163.com>
2024-03-23 09:37:27 +0800

    Complete the overall framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { ReactNode } from "react";
 import styled from "styled-components";
+import {
+  bgGrey,
+  white,
+  textGrey,
+  borderGrey,
+  black,
+  avatarGrey,
+  toolTipBgColor,
+  inputBorderRadius,
+} from "./const";
 
 function App() {
-  const grey = "#2b3138";
-  const white = "#fff";
-  const textGrey = "#657485";
-
   const Modal = styled.section`
     width: 28.5rem;
     height: 31.56rem;
@@ -20,27 +27,37 @@ function App() {
     display: flex;
     align-items: flex-start;
   `;
-  const Button = styled.button`
-    background-color: ${grey};
+  type ButtonPropsTypes = {
+    $bgColor?: string;
+    $fontColor?: string;
+    $fontSize?: string;
+    $width?: string;
+    $height?: string;
+  };
+  const Button = styled.button<ButtonPropsTypes>`
+    background-color: ${(props) => props.$bgColor ?? bgGrey};
     border: none;
-    border-radius: 0.3rem;
-    width: 2rem;
-    height: 2rem;
+    border-radius: ${inputBorderRadius};
+    width: ${(props) => props.$width ?? "2rem"};
+    height: ${(props) => props.$height ?? "2rem"};
     margin: 0.8rem 0.8rem 0 auto;
     cursor: pointer;
+    color: ${(props) => props.$fontColor ?? white};
+    font-size: ${(props) => props.$fontSize ?? "1rem"};
   `;
 
-  const Pane = styled.div`
+  const Pane = styled.div<{ $isColumn?: boolean }>`
     display: flex;
     margin: 1.5rem 1rem 1rem 1rem;
     align-items: center;
+    flex-direction: ${(props) => (props.$isColumn ? "column" : "row")};
   `;
 
   const AccountToIcon = styled.div`
     flex: 1;
     font-size: 0.7rem;
     margin: 2rem 0 0 0.5rem;
-    color: #657485;
+    color: ${textGrey};
   `;
   const AccountSection = styled.div<{ $flex?: string; $height?: string }>`
     flex: ${(props) => props.$flex};
@@ -48,62 +65,146 @@ function App() {
     border-radius: 1rem;
     height: ${(props) => props.$height};
   `;
-  const TitleSection = styled.div`
+  const Title = styled.div`
     margin-bottom: 0.8rem;
     font-weight: bold;
-    color: #657485;
+    color: ${textGrey};
+  `;
+  const SubTitle = styled(Title)`
+    font-weight: normal;
+    font-size: 0.7rem;
+  `;
+  const Container = styled(Pane)<{ $justifyContent?: string }>`
+    align-items: baseline;
+    margin: 0;
+    justify-content: ${(props) => props.$justifyContent ?? "flex-start"};
+    width: 100%;
   `;
   const InputContainer = styled.div`
-    border-radius: 0.3rem;
+    border-radius: ${inputBorderRadius};
     display: flex;
     align-items: center;
-    background-color: ${(props) => props.color ?? "#151317"};
+    background-color: ${(props) => props.color ?? black};
     height: 3rem;
   `;
-  const Avatar = styled.div`
+  const Avatar = styled.div<{ $color: string; $size?: string }>`
     border-radius: 50rem;
     width: 1.7rem;
     height: 1.7rem;
     margin-left: 1rem;
     margin-right: 0.5rem;
-    background-color: ${(props) => props.color};
+    background-color: ${(props) => props.$color};
+    padding: ${(props) => props.$size ?? 0};
   `;
   const AccountHash = styled.div<{ $fontSize?: string }>`
     flex: 8;
     color: ${textGrey};
-    font-size: ${(props) => props.$fontSize};
+    font-size: ${({ $fontSize }) => $fontSize};
   `;
-  const AccountIconPen = styled.div`
+
+  const InputContainerMax = styled(InputContainer)`
+    width: 100%;
+    background: none;
+    border: 0.12rem solid ${borderGrey};
+    height: 4rem;
+  `;
+  const ToolTip = styled.div<{ $lastElement?: boolean }>`
+    color: ${textGrey};
+    background-color: ${toolTipBgColor};
+    padding: 0.3rem 0.5rem;
+    font-size: 0.7rem;
+    border-radius: ${inputBorderRadius};
+    margin-right: ${(props) => (props.$lastElement ? "0" : "0.6rem")};
+    margin-top: 0.5rem;
+    font-weight: bold;
+  `;
+
+  const Indicator = styled.div`
+    background-color: ${black};
+    width: 100%;
+    height: 2.3rem;
+    border-radius: ${inputBorderRadius};
+    margin-bottom: 0.2rem;
+  `;
+
+  const Link = ({
+    children,
+    className,
+  }: {
+    children: ReactNode;
+    className?: string;
+  }) => <a className={className}>{children}</a>;
+  const StyledLink = styled(Link)`
+    color: ${textGrey};
+    margin-top: 0.5rem;
+    font-size: 0.8rem;
+    cursor: pointer;
+  `;
+
+  const IconContainer = styled.div`
     margin: 1rem;
     cursor: pointer;
+  `;
+  const Image = styled.img`
+    width: 0.75rem;
   `;
 
   return (
     <Modal>
       <Header>
         <HeaderTitle>Deposit Atom</HeaderTitle>
-        <Button>X</Button>
+        <Button $fontColor="white">X</Button>
       </Header>
 
       <Pane>
         <AccountSection $flex="10">
-          <TitleSection>From Cosmos Hub</TitleSection>
-          <InputContainer color="#151317">
-            <Avatar color="#3a394c" />
+          <Title>From Cosmos Hub</Title>
+          <InputContainer color={`${black}`}>
+            <Avatar $color={`${avatarGrey}`} />
             <AccountHash $fontSize="0.7rem">atom1xy5y...m6wwz9a</AccountHash>
           </InputContainer>
         </AccountSection>
         <AccountToIcon>→</AccountToIcon>
         <AccountSection $flex="7">
-          <TitleSection>From Cosmos Hub</TitleSection>
-          <InputContainer color="#151317">
-            <Avatar color="#fff" />
+          <Title>From Cosmos Hub</Title>
+          <InputContainer color={`${black}`}>
+            <Avatar $color={`${white}`} />
             <AccountHash $fontSize="0.7rem">atom1xy5y...m6wwz9a</AccountHash>
-            <AccountIconPen>
-              <img src="../public/pencil.png" width="12" />
-            </AccountIconPen>
+            <IconContainer>
+              <Image src="pencil.png" />
+            </IconContainer>
           </InputContainer>
         </AccountSection>
+      </Pane>
+
+      <Pane $isColumn={true}>
+        <Container $justifyContent="space-between">
+          <Title>Select Amount</Title>
+          <SubTitle>Avaliable 2 ATOM</SubTitle>
+        </Container>
+        <InputContainerMax>
+          <Container>
+            <Avatar $color={`${avatarGrey}`} $size="0.43rem" />
+          </Container>
+        </InputContainerMax>
+        <Container $justifyContent="flex-end">
+          <ToolTip>Max</ToolTip>
+          <ToolTip>1/2</ToolTip>
+          <ToolTip $lastElement={true}>1/3</ToolTip>
+        </Container>
+      </Pane>
+
+      <Pane $isColumn={true}>
+        <Indicator></Indicator>
+        <Button
+          $bgColor={`${white}`}
+          $fontColor={`${black}`}
+          $width="100%"
+          $height="3.8rem"
+        >
+          Transfer
+        </Button>
+        <StyledLink>Cancel</StyledLink>
       </Pane>
     </Modal>
   );
---
+++ src/const.tsx
@@ -0,0 +1,8 @@
+export const bgGrey = "#2b3138";
+export const white = "#fff";
+export const textGrey = "#657485";
+export const borderGrey = "#2c3239";
+export const black = "#151317";
+export const avatarGrey = "#3a394c";
+export const toolTipBgColor = "#2b3138";
+export const inputBorderRadius = "0.3rem";
